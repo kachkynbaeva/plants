1d3091dc5e03374225bf3073f221212f88e85f6c
kachkynbaeva <kunduz1604@icloud.com>
2023-10-24 22:51:45 +0600

    finished

---
+++ style/script.js
@@ -0,0 +1,8 @@
+const mobileBtn =document.querySelector('.mobil-btn')
+const mobileMenu =document.querySelector('.mobile-menu')
+const header =document.querySelector('.header')
+const body  =document.querySelector('.body')
+
+mobileBtn
+
+
